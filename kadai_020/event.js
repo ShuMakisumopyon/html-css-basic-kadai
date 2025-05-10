2c08d4e9e882dbc02e57e1b675b3a40d8e69a02e
ShuMakisumopyon <shuuu0808a@gmail.com>
2025-05-10 19:07:42 +0900

    kadai_020の追加

---
+++ kadai_020/event.js
@@ -0,0 +1,16 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+
+  if () {
+    const text = document.getElementById('text');
+    textContent ="";
+    
+  }
+
+  else {
+
+  }
+
+
+});
